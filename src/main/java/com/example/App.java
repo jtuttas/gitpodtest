bbe98db59eadcadf37b7dc9b15169eae6897aaed
jtuttas <jtuttas@gmx.net>
2021-09-11 16:46:50 +0200

    init

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,15 @@
+package com.example;
+
+/**
+ * Hello world!
+ */
+public class App {
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hallo Welt");        
+    }
+}
